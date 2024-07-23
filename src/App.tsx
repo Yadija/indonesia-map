65ee280a1bb6df152c6db863473a97ea61103767
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-07-23 14:21:32 +0800

    ⚒️build: add indonesia map by components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,77 @@
-import './App.css';
+import Aceh from './components/Aceh';
+import Bali from './components/Bali';
+import Banten from './components/Banten';
+import Bengkulu from './components/Bengkulu';
+import Gorontalo from './components/Gorontalo';
+import Jakarta from './components/Jakarta';
+import Jambi from './components/Jambi';
+import JawaBarat from './components/JawaBarat';
+import JawaTengah from './components/JawaTengah';
+import JawaTimur from './components/JawaTimur';
+import KalimantanBarat from './components/KalimantanBarat';
+import KalimantanSelatan from './components/KalimantanSelatan';
+import KalimantanTengah from './components/KalimantanTengah';
+import KalimantanTimur from './components/KalimantanTimur';
+import KalimantanUtara from './components/KalimantanUtara';
+import KepulauanBangkaBelitung from './components/KepulauanBangkaBelitung';
+import KepulauanRiau from './components/KepulauanRiau';
+import Lampung from './components/Lampung';
+import Maluku from './components/Maluku';
+import MalukuUtara from './components/MalukuUtara';
+import NusaTenggaraBarat from './components/NusaTenggaraBarat';
+import NusaTenggaraTimur from './components/NusaTenggaraTimur';
+import Papua from './components/Papua';
+import PapuaBarat from './components/PapuaBarat';
+import Riau from './components/Riau';
+import SulawesiBarat from './components/SulawesiBarat';
+import SulawesiSelatan from './components/SulawesiSelatan';
+import SulawesiTengah from './components/SulawesiTengah';
+import SulawesiTenggara from './components/SulawesiTenggara';
+import SulawesiUtara from './components/SulawesiUtara';
+import SumateraBarat from './components/SumateraBarat';
+import SumateraSelatan from './components/SumateraSelatan';
+import SumateraUtara from './components/SumateraUtara';
+import Yogyakarta from './components/Yogyakarta';
 
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <section className='grid h-screen place-items-center'>
+      <svg width='1000' height='350'>
+        <Aceh />
+        <Bali />
+        <Banten />
+        <Bengkulu />
+        <Gorontalo />
+        <Jakarta />
+        <Jambi />
+        <JawaBarat />
+        <JawaTengah />
+        <JawaTimur />
+        <KalimantanBarat />
+        <KalimantanSelatan />
+        <KalimantanTengah />
+        <KalimantanTimur />
+        <KalimantanUtara />
+        <KepulauanBangkaBelitung />
+        <KepulauanRiau />
+        <Lampung />
+        <Maluku />
+        <MalukuUtara />
+        <NusaTenggaraBarat />
+        <NusaTenggaraTimur />
+        <Papua />
+        <PapuaBarat />
+        <Riau />
+        <SulawesiBarat />
+        <SulawesiTengah />
+        <SulawesiTenggara />
+        <SulawesiSelatan />
+        <SulawesiUtara />
+        <SumateraBarat />
+        <SumateraSelatan />
+        <SumateraUtara />
+        <Yogyakarta />
+      </svg>
+    </section>
   );
 }
-
-export default App;
