daf3298aeb5f72e7338f3d633bd60ff5b3401030
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-09-28 17:34:34 +0800

    ⚒️build: add toast when load app

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,26 @@ import InfoSheet from './components/InfoSheet';
 import SVGElement from './components/SVGElement';
 // data
 import indonesiaMap from './data/indonesia-map.json';
+// hooks
+import { useToast } from './hooks/use-toast';
 // interfaces
 import { IProvince } from './interfaces/IProvince';
 
 export default function App() {
   const [open, setOpen] = useState(false);
   const [province, setProvince] = useState<IProvince | null>(null);
+  const [toastShown, setToastShown] = useState(true);
+
+  const { toast } = useToast();
+  if (toastShown) {
+    toast({
+      title: 'Info',
+      description: 'Peta ini berasal dari simplemaps.com',
+      duration: 10000,
+    });
+
+    setToastShown(false);
+  }
 
   const handlePathClick = (id: string) => {
     const selectedProvince = indonesiaMap.find((map) => map.id === id);
